57cbed5bce619c645b24b81f084e17540d735d3a
lssdavies <91108369+IslandBoyL@users.noreply.github.com>
2022-03-21 12:01:20 -0700

    reverting to a previous commit to resolve issue with app

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import "./App.css";
 import Header from './components/Header';
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Footer from "./components/Footer";
+// import Hero from "./components/Hero";
+
 
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <Hero />
+      {/* <Hero /> */}
       <Routes>
-        <Route path='/' element = {<About />}/>
+        <Route path='/' />
       </Routes>
-      <Footer />
     </Router>
    );
 }
