11b0cedb0b6a830d5ea0ac1d152828a067ca9091
lssdavies <91108369+IslandBoyL@users.noreply.github.com>
2022-03-21 13:58:30 -0700

    issue resolved

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,39 +1,9 @@
 import React from 'react';
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FaBars, FaSuitcase } from "react-icons/fa";
-import { AiOutlineClose, AiFillContacts, AiFillFilePdf } from "react-icons/ai";
+import { FaSuitcase } from "react-icons/fa";
+import { AiFillContacts, AiFillFilePdf } from "react-icons/ai";
 import { SiAboutdotme } from "react-icons/si";
 import "../../../src/assets/styles/index.css";
-
-
-//array to store menu paths, classNames and Icons
- const menuLink = [
-   {
-     link: "About Me",
-     path: "/",
-     icon: <SiAboutdotme />,
-     class: "menuItem",
-   },
-   {
-     link: "Portfolio",
-     path: "/portfolio",
-     icon: <FaSuitcase />,
-     class: "menuItem",
-   },
-   {
-     link: "Contact",
-     path: "/contact",
-     icon: <AiFillContacts />,
-     class: "menuItem",
-   },
-   {
-     link: "Resume",
-     path: "/resume",
-     icon: <AiFillFilePdf />,
-     class: "menuItem",
-   },
- ];
 
 
 const Navbar = () => {
